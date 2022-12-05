6328db998b2e26d33c5f8cd2a3a5a2fa6b6ece44
juan schenfeld <juan-schenfeld-p@outlook.com>
2022-12-05 11:07:50 -0300

    added DashboardPage class and validateLogin mathod

---
+++ src/main/java/DashboardPage.java
@@ -0,0 +1,11 @@
+import org.openqa.selenium.WebDriver;
+
+public class DashboardPage extends BasePage{
+    public DashboardPage(WebDriver driver) {
+        super(driver);
+    }
+
+    public boolean validateLogin(){
+        return driver.getCurrentUrl().equals("https://opensource-demo.orangehrmlive.com/web/index.php/dashboard/index");
+    }
+}
