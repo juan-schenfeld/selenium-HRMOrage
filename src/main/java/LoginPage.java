60a37f2018c0624514f959f59ad5cf7050178e5c
juan schenfeld <juan-schenfeld-p@outlook.com>
2022-12-05 10:29:52 -0300

    added LoginPage class, and a basic automation

--- src/main/java/LoginPage.java
+++ src/main/java/LoginPage.java
@@ -1,2 +1,28 @@
-public class LoginPage {
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class LoginPage extends BasePage{
+
+    @FindBy(xpath="input[name='username']")
+    private WebElement usernameField;
+    @FindBy(xpath="input[name='password']")
+    private WebElement passwordField;
+    @FindBy(xpath="button[type='submit']")
+    private WebElement loginButton;
+
+    public LoginPage(WebDriver driver) {
+        super(driver);
+    }
+
+    public void login(String username, String password){
+        usernameField.sendKeys(username);
+        passwordField.sendKeys(password);
+        loginButton.click();
+    }
+
+    public void defaultLogin(){
+        login("Admin", "admin123");
+    }
+
 }
