1e1ccf6883d1e6f469a6a2e8dd85ea6b35c95cbd
juan schenfeld <juan-schenfeld-p@outlook.com>
2022-12-05 11:18:40 -0300

    added BaseTest with instanciation in chrome using WebDriverManager dependency; added testDefaultLogin;

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -1,2 +1,28 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.BeforeMethod;
+
 public class BaseTest {
+
+    private WebDriver driver;
+    protected LoginPage homePage;
+
+    private void getDriver(){
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver(getChromeOptions());
+        homePage = new LoginPage(driver);
+    }
+
+    @BeforeMethod
+    public void setUp(){
+        getDriver();
+        driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+    }
+
+    private ChromeOptions getChromeOptions(){
+        return new ChromeOptions()
+                .setHeadless(true);
+    }
 }
--- src/test/java/LoginTests.java
+++ src/test/java/LoginTests.java
@@ -1,2 +1,18 @@
-public class LoginTests {
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertTrue;
+
+public class LoginTests extends BaseTest{
+
+    @Test
+    public void defaultLogin(){
+        boolean logged = homePage.defaultLogin().validateLogin();
+        assertTrue(logged);
+    }
+
+
+
+
+
+
 }
