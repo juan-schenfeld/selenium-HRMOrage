c5fac0ad2e46dc097c86ceabfe18e167cb780f8e
juan schenfeld <juan-schenfeld-p@outlook.com>
2022-12-08 10:32:54 -0300

    added method getText with explicit wait

--- src/main/java/BasePage.java
+++ src/main/java/BasePage.java
@@ -9,33 +9,43 @@ import java.time.Duration;
 class BasePage {
     protected WebDriver driver;
 
-    public BasePage(WebDriver driver){
+    public BasePage(WebDriver driver) {
         this.driver = driver;
         PageFactory.initElements(driver, this);
     }
 
-    protected WebElement visibilityOf(WebElement element, int seconds){
+    protected WebElement visibilityOf(WebElement element, int seconds) {
         return new WebDriverWait(driver, Duration.ofSeconds(seconds)).until(ExpectedConditions.visibilityOf(element));
     }
-    protected WebElement visibilityOf(WebElement element){
+
+    protected WebElement visibilityOf(WebElement element) {
         return visibilityOf(element, 5);
     }
 
-    protected void click(WebElement element, int seconds){
+    protected void click(WebElement element, int seconds) {
         new WebDriverWait(driver, Duration.ofSeconds(seconds)).until(ExpectedConditions.elementToBeClickable(element)).click();
     }
 
-    protected void click(WebElement element){
+    protected void click(WebElement element) {
         click(element, 5);
     }
 
-    protected void sendKeys(WebElement element, String keys, int seconds){
+    protected void sendKeys(WebElement element, String keys, int seconds) {
         new WebDriverWait(driver, Duration.ofSeconds(seconds)).until(ExpectedConditions.visibilityOf(element)).clear();
         element.sendKeys(keys);
     }
 
-    protected void sendKeys(WebElement element, String keys){
+    protected void sendKeys(WebElement element, String keys) {
         sendKeys(element, keys, 5);
     }
 
+
+    protected String getText(WebElement element, int seconds) {
+        return new WebDriverWait(driver, Duration.ofSeconds(seconds))
+                .until(ExpectedConditions.visibilityOf(element)).getText();
+    }
+
+    protected String getText(WebElement element) {
+        return getText(element, 5);
+    }
 }
--- src/test/java/AddEmployeeTests.java
+++ src/test/java/AddEmployeeTests.java
@@ -1,2 +1,15 @@
-public class AddEmployeeTests {
+import org.testng.annotations.Test;
+
+public class AddEmployeeTests extends BaseTest{
+
+    @Test
+    public void testAddEmployeeWithCredentials(){
+        homePage.defaultLogin().goToPIM().addEmployeePage()
+                .fillFullName("Bruno", "Diaz")
+                .clickLoginDetailsCheckbox().setUsername("bruno.diaz")
+                .setId("999")
+                .setPassword("Qwerty1@").confirmPassword("Qwerty1@")
+                .clickSave();
+    }
+
 }
