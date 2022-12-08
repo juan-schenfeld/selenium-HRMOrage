4fa10b5097ed17e1c13979d43178fe7b1be65133
juan schenfeld <juan-schenfeld-p@outlook.com>
2022-12-08 10:43:41 -0300

    added 2 tests to AddEmployeeTests class

--- src/test/java/AddEmployeeTests.java
+++ src/test/java/AddEmployeeTests.java
@@ -1,15 +1,41 @@
 import org.testng.annotations.Test;
 
+import java.io.File;
+
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertTrue;
+
 public class AddEmployeeTests extends BaseTest{
 
     @Test
     public void testAddEmployeeWithCredentials(){
-        homePage.defaultLogin().goToPIM().addEmployeePage()
-                .fillFullName("Bruno", "Diaz")
+        String firstName = "Bruno";
+        String middleName = "";
+        String lastName = "Diaz";
+
+        File photo = new File("src/test/resources/bruno.diaz.jpg");
+        var AddEmployeePage = homePage.defaultLogin().goToPIM().addEmployeePage()
+                .fillFullName(firstName, middleName, lastName)
+                .setId("999").addPhoto(photo)
                 .clickLoginDetailsCheckbox().setUsername("bruno.diaz")
-                .setId("999")
-                .setPassword("Qwerty1@").confirmPassword("Qwerty1@")
-                .clickSave();
+                .setPassword("Qwerty1@").confirmPassword("Qwerty1@");
+        var employeeDetailsPage = AddEmployeePage.clickSave();
+        assertTrue(AddEmployeePage.theEmployeeHasAdded());
+        assertEquals(employeeDetailsPage.getEmployeeFullName(), firstName + " " + lastName);
+    }
+
+    @Test
+    public void testAddEmployeeWithoutCredentials(){
+        String firstName = "Luke";
+        String middleName = "";
+        String lastName = "Skywalker";
+        File photo = new File("src/test/resources/luke.skywalker.jpeg");
+        var AddEmployeePage = homePage.defaultLogin().goToPIM().addEmployeePage()
+                .fillFullName(firstName, middleName, lastName)
+                .setId("").addPhoto(photo);
+        var employeeDetailsPage = AddEmployeePage.clickSave();
+        assertTrue(AddEmployeePage.theEmployeeHasAdded());
+        assertEquals(employeeDetailsPage.getEmployeeFullName(), firstName + " " + lastName);
     }
 
 }
